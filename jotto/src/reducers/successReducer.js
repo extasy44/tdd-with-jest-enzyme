103bd6484e094b07f902a6b680bdcb16eb88dc89
extasy44 <seoheejun@gmail.com>
2020-11-02 00:50:05 +1100

    write code for testing successReducer

--- jotto/src/reducers/successReducer.js
+++ jotto/src/reducers/successReducer.js
@@ -1,3 +1,5 @@
+import { actionTypes } from '../actions';
+
 /**
  * @function successReducer
  * @param { array } state = Array of guessed words.
@@ -5,6 +7,14 @@
  * @returns { boolean } - new success state.
  */
 
-export default (state, action) => {
-  return null;
+const successReducer = (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+
+    default:
+      return state;
+  }
 };
+
+export default successReducer;
