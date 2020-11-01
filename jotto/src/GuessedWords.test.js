456a69140e02b5d90121aa78ba309c076ce9d685
extasy44 <seoheejun@gmail.com>
2020-11-01 22:44:02 +1100

    Test and code for non-zero-guessed-word

--- jotto/src/GuessedWords.test.js
+++ jotto/src/GuessedWords.test.js
@@ -40,5 +40,27 @@ describe('if there are no words guessed ', () => {
 });
 
 describe('if there are words guessed ', () => {
-  test('renders without error', () => {});
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 },
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guess-words');
+    expect(guessedWordsNode.text().length).toBe(1);
+  });
+
+  test('correct number of guessed words', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordNodes.length).toBe(guessedWords.length);
+  });
 });
