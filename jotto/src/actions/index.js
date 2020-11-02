5491ba25a72e4264b3c620bf2d5961e63672ad34
extasy44 <seoheejun@gmail.com>
2020-11-03 01:14:01 +1100

    setup guessedWord tests

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,7 +1,19 @@
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD',
 };
 
 export const correctGuess = () => {
   return { type: actionTypes.CORRECT_GUESS };
+};
+
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ * and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param { string } guessedWord = Guessed word.
+ * @returns { function } - Redux Thunk function
+ */
+export const guessWord = (guessedWord) => {
+  return (dispatch, getState) => {};
 };
