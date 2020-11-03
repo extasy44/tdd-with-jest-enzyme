297762143270fad9a4ec4820bde2325d891b0eeb
extasy44 <seoheejun@gmail.com>
2020-11-03 22:56:54 +1100

    Add Thunk for `guessedWords` picece of state

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,3 +1,5 @@
+import { getLetterMatchCount } from '../helpers';
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
@@ -15,5 +17,17 @@ export const correctGuess = () => {
  * @returns { function } - Redux Thunk function
  */
 export const guessWord = (guessedWord) => {
-  return (dispatch, getState) => {};
+  return (dispatch, getState) => {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
+  };
 };
--- jotto/src/reducers/index.js
+++ jotto/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import success from './successReducer';
 import guessedWords from './guessedWordsReducer';
+import secretWordReducer from './secretWordReducer';
 
 export default combineReducers({
   success,
   guessedWords,
+  secretWordReducer,
 });
